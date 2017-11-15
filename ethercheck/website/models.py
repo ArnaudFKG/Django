ef39d7d2d0afa9901b7a540cf365e447882c20d2
Arnaud Fickinger <arnaud.fickinger@polytechnique.edu>
2017-11-15 12:27:23 +0100

    add address table

--- ethercheck/website/models.py
+++ ethercheck/website/models.py
@@ -7,12 +7,15 @@ class Transaction(models.Model):
 	source = models.CharField("From", max_length=64 , default="", blank=True)
 	hashCode = models.CharField("Hash", max_length=64 , default="", blank=True)
 	destination = models.CharField("To", max_length=64 , default="", blank=True)
+
 	
 	
 	def __str__(self):
 		return self.hashCode
 	
 	
-	
-	
-	
+class Address(models.Model):
+	publicHash = models.CharField(max_length=64)
+	ethBalance = models.FloatField(blank=True)
+	usdBalance = models.FloatField(blank=True)
+	nbTrx = models.IntegerField(blank=True)
